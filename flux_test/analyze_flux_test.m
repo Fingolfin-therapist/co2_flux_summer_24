945a201e3a18dc5b998e20db0adba29dbbb9629f
Lincoln Scheer <63661070+BikeLinc@users.noreply.github.com>
2024-05-20 13:18:25 -0600

    Upload Initial Flux Test Data

--- flux_test/analyze_flux_test.m
+++ flux_test/analyze_flux_test.m
@@ -10,6 +10,20 @@ data.T = datetime( data.DATE + timeofday(data.TIME) , 'Format', 'default');
 data.DATE = [];
 data.TIME = [];
 data = rmmissing(data);
+
+
 %% Plot Data
 
 plot(data.T, movmean(data.C, 100))
+
+%% Load Dataset
+
+table = importdaqfile("initial_daq_data.txt");
+
+figure()
+plot(table.T, table.CA)
+hold on
+plot(table.T, table.CB)
+legend("Ambient", "Chamber")
+
+
