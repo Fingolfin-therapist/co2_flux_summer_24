346607f0f5bd6566e17c1cace660f69bdc2500ba
Lincoln Scheer <63661070+BikeLinc@users.noreply.github.com>
2024-05-20 16:08:24 -0600

    Add Steady State Concentration to Flux Test

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# co2_flux_summer_24
+# Wildfire CO<sub>2</sub> Flux Project - Summer 2024
+This directory includes scripts and data for the 2024 Summer Wildfire CO<sub>2</sub> Flux Project. Please see the repository structure section below.
+
+# Repository Outline
+
+**Ambient Colocation - `/ambient_colocation`**
+
+Contains analysis scripts and datasets for greenhouse ambient colocation between ELT CO2 sensors and the Picarro instrument.
+
+**Flux Test - `/flux_test`**
+
+Contains set-point calculation scripts, analysis scripts, and datasets for the 1st lab flux test.
+
+**Lab Colocation - `/lab_colocation`**
+
+Contains analysis scripts and datasets for the lab colocation (aka dilution test) between ELT CO2 sensors and the LICOR instrument.
+
+**Measurement Range - `/measurement_range`**
+
+Contains scripts that simulate the mass balance model equation for a dynamic flux chamber.
+
+**MFC 2.5 LPM Calibration w/ 400 ppm CO2 - `/mfc_calibration`**
+
+Contains datasets for calibrating the ALICAT 2.5 L/min Mass Flow Controller for gas mixture of 400 ppm CO2, 2 ppm CH4, balanced.
+
+# Questions
+Please email Lincoln (lascheer@fortlewis.edu) for any questions about this repository.
--- flux_test/analyze_flux_test.m
+++ flux_test/analyze_flux_test.m
@@ -3,27 +3,59 @@
 %
 clear, clc, close all
 
-%% Import
+%% Import LICOR Dataset
 
-data = IMPORTLICOR("5_17_2024_LICOR.data");
-data.T = datetime( data.DATE + timeofday(data.TIME) , 'Format', 'default');
-data.DATE = [];
-data.TIME = [];
-data = rmmissing(data);
+licor = IMPORTLICOR("licor_lab_flux_test_data.data");
+licor.T = datetime( licor.DATE + timeofday(licor.TIME) , 'Format', 'default');
+licor.DATE = [];
+licor.TIME = [];
+licor = rmmissing(licor);
+licor = table2timetable(licor);
 
+%% Import DAQ Dataset
+
+daq = importdaqfile('daq_lab_flux_test_data.txt');
+
+%% First Test
+
+%% Define Date Range - Test 1
+tStart1 = datetime("5/20/2024  10:08:00 AM", "InputFormat","MM/dd/uuuu hh:mm:ss aa");
+tEnd1 = datetime("5/20/2024  10:24:00 AM", "InputFormat","MM/dd/uuuu hh:mm:ss aa");
+F = 0.5e-6;    % umol/m^2/s
+As = 
+
+%% Define Date Range - Test 2
+tStart2 = datetime("5/20/2024  10:42:00 AM", "InputFormat","MM/dd/uuuu hh:mm:ss aa");
+tEnd2 = datetime("5/20/2024  11:01:00 AM", "InputFormat","MM/dd/uuuu hh:mm:ss aa");
+
+tStart = tStart2;
+tEnd = tEnd2;
+
+
+% licor was observed to have a 3 minute offset from current time while
+% sampling.
+licorTimeOffset = -minutes(3);
+
+% shrink both datasets to range
+licor_idx = licor.T < tEnd + licorTimeOffset & licor.T > tStart + licorTimeOffset;
+licor = licor(licor_idx, :);
+daq_idx = daq.T < tEnd & daq.T > tStart;
+daq = daq(daq_idx, :);
+
+clear licor_idx daq_idx
 
 %% Plot Data
-
-plot(data.T, movmean(data.C, 100))
-
-%% Load Dataset
-
-table = importdaqfile("initial_daq_data.txt");
-
-figure()
-plot(table.T, table.CA)
+fig2 = figure();
 hold on
-plot(table.T, table.CB)
-legend("Ambient", "Chamber")
+plot(licor.T, movmean(licor.C, 10));
+title("Failed - Lab Flux Test Results")
+xlabel("Time")
+ylabel("CO_2 [ppm]")
+grid on
+
+
+
+
+
 
 
