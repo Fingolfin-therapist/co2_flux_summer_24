bff95d2f76371e11c138c70a0b1441b470f6de83
Scheer <lascheer@fortlewis.edu>
2024-05-23 13:06:42 -0600

    Working on Flux Script - Improving Calibration Application

--- flux_test/analysis.m
+++ flux_test/analysis.m
@@ -119,16 +119,16 @@ corrected_corr_data = rmmissing(corrected_corr_data);
     
 figure();
 hold on;
-plot(corrected_corr_data.T, corrected_corr_data.C, 'b', 'DisplayName', 'LICOR C');
-plot(corrected_corr_data.T, corrected_corr_data.CB, 'g', 'DisplayName', 'Corr. DAQ CB');
-plot(corrected_corr_data.T, corrected_corr_data.CA, 'r', 'DisplayName', 'Corr. DAQ CA');
-plot(corr_data.T, corr_data.CB, 'c', 'DisplayName', 'DAQ CB')
-plot(corr_data.T, corr_data.CA, 'm', 'DisplayName', 'DAQ CA')
+plot(corrected_corr_data.T, corrected_corr_data.C, 'b', 'DisplayName', 'Raw LICOR');
+plot(corrected_corr_data.T, corrected_corr_data.CB, 'g', 'DisplayName', 'Corrected DAQ Chamber');
+plot(corrected_corr_data.T, corrected_corr_data.CA, 'r', 'DisplayName', 'Corrected DAQ Ambient');
+plot(corr_data.T, corr_data.CB, 'c', 'DisplayName', 'Raw DAQ Chamber')
+plot(corr_data.T, corr_data.CA, 'm', 'DisplayName', 'Raw DAQ Ambient')
 ylabel('CO_2 [ppm]');
 legend();
-title(['Automatic Time-Correction of Flux Test Dataset ' dataset]);
+title(["Automatic Timestamp Correction" "[DATASET " + dataset + "]"]);
 xlabel('Time');
-
+grid on
 hold off;
 corr_data = corrected_corr_data;
 
@@ -136,20 +136,21 @@ corr_data = corrected_corr_data;
 
 load('calib.mat')
 
-corr_data.CB_CALIB = ann_regb([corr_data.CB, corr_data.TB, corr_data.HB]')';
-corr_data.CA_CALIB = ann_rega([corr_data.CA, corr_data.TA, corr_data.HA]')';
+corr_data.CB_CALIB = predict(lin_rega, [corr_data.CB, corr_data.TB, corr_data.HB]);
+corr_data.CA_CALIB = predict(lin_regb, [corr_data.CA, corr_data.TA, corr_data.HA]);
+%corr_data.CB_CALIB = ann_regb([corr_data.CB, corr_data.TB, corr_data.HB]')';
+%corr_data.CA_CALIB = ann_rega([corr_data.CA, corr_data.TA, corr_data.HA]')';
 
 figure();
 hold on;
-plot(corr_data.T, corr_data.CB_CALIB, 'g', 'DisplayName', 'Corr. DAQ CB');
-plot(corr_data.T, corr_data.CA_CALIB, 'r', 'DisplayName', 'Corr. DAQ CA');
-plot(corr_data.T, corr_data.CB, 'c', 'DisplayName', 'DAQ CB')
-plot(corr_data.T, corr_data.CA, 'm', 'DisplayName', 'DAQ CA')
+plot(corr_data.T, corr_data.CB_CALIB, 'g', 'DisplayName', 'Corrected DAQ Chamber');
+plot(corr_data.T, corr_data.CA_CALIB, 'r', 'DisplayName', 'Corrected DAQ Ambient');
+plot(corr_data.T, corr_data.CB, 'c', 'DisplayName', 'DAQ Chamber')
+plot(corr_data.T, corr_data.CA, 'm', 'DisplayName', 'DAQ Ambient')
 ylabel('CO_2 [ppm]');
 legend();
-title(['Calibrations of ELT Sensors of Flux Test Dataset ' dataset]);
+title(["Calibrations Applied to Flux Dataset" "[DATASET " + dataset + "]"]);
 xlabel('Time');
-
 hold off;
 
 
@@ -158,20 +159,22 @@ hold off;
 data = [];
 for dataset_idx = 1:height(map)
     
+    % get timestamps
     tStart = map{dataset_idx, 8};
     tStartLicor = map{dataset_idx, 10};
     tEnd = map{dataset_idx, 9};
     tEndLicor = map{dataset_idx, 11};
     
+    % select setpoint
     data_idx = corr_data.T < tEndLicor  & corr_data.T > tStartLicor;
     dataTmp = corr_data(data_idx, :);
     data = dataTmp;
     
+    % apply moving average
+    data.CB = movmean(data.CB, 6);
+    data.CA = movmean(data.CA, 6);
     
-    data.CB = movmean(data.CB, 10);
-    data.CA = movmean(data.CA, 10);
-    
-    
+    % plot raw set-point dataset
     figure();
     hold on
     plot(data.T, data.C, 'b', 'DisplayName', 'LICOR Reference')
@@ -179,14 +182,28 @@ for dataset_idx = 1:height(map)
     plot(data.T, data.CA, 'r-', 'DisplayName', 'DAQ Ambient')
     ylabel('CO_2 [ppm]');
     legend();
-    title("Flux Test Dataset " + dataset + " - " + map{dataset_idx, 4} +" umol/m^2/s" );
+    title(["Raw Data - Delivering " + map{dataset_idx, 4} + " μmol/m^2/s", "[DATASET " + dataset + "]"]);
     xlabel('Time');
     hold off
     
+    % plot calibrated set-point dataset
+    figure();
+    hold on
+    plot(data.T, data.C, 'b', 'DisplayName', 'LICOR Reference')
+    plot(data.T, data.CB_CALIB, 'g', 'DisplayName', 'Corrected DAQ Chamber')
+    plot(data.T, data.CA_CALIB, 'r-', 'DisplayName', 'Corrected DAQ Ambient')
+    ylabel('CO_2 [ppm]');
+    legend();
+    title(["Calibrated Data - Delivering " + map{dataset_idx, 4} + " μmol/m^2/s", "[DATASET " + dataset + "]"]);
+    xlabel('Time');
+    hold off
+
+    % calculate floor dataset, because we are looking for offsets
     data.C_FLOOR = data.C - min(data.C);
     data.CB_FLOOR = data.CB - min(data.CB);
     data.CA_FLOOR = data.CA - min(data.CA);
 
+    % plot floored dataset
     figure();
     hold on;
     plot(data.T, data.CB_FLOOR, 'g', 'DisplayName', 'DAQ CB');
@@ -194,43 +211,67 @@ for dataset_idx = 1:height(map)
     plot(data.T, data.C_FLOOR, 'c', 'DisplayName', 'LICOR')
     ylabel('CO_2 [ppm]');
     legend();
-    title(['Floored ELT Sensors of Flux Test Dataset ' dataset]);
+    title(["Floored Data - Delivering " + map{dataset_idx, 4} + " μmol/m^2/s", "[DATASET " + dataset + "]"]);
     xlabel('Time');
     hold off;
     
-    
+    % calculate steady state indices
     thresh = 0.05;
     cb_ss = find(abs(diff(movmean(data.CB, 100)))<thresh);
     c_ss = find(abs(diff(movmean(data.C, 100)))<thresh);
     
+    % plot steady state indices
     figure();
     hold on;
-    plot(data.T(cb_ss), data.CB_FLOOR(cb_ss), 'g.', 'DisplayName', 'DAQ CB');
-    plot(data.T(cb_ss), data.CA_FLOOR(cb_ss), 'r.', 'DisplayName', 'DAQ CA');
-    plot(data.T(cb_ss), data.C_FLOOR(cb_ss), 'c.', 'DisplayName', 'LICOR')
+    plot(data.T(c_ss), data.CB(c_ss), 'gd', 'DisplayName', 'Steady-State DAQ Chamber');
+    plot(data.T(cb_ss), data.CA(cb_ss), 'rd', 'DisplayName', 'Steady-State DAQ Ambient');
+    plot(data.T(cb_ss), data.C(cb_ss), 'cd', 'DisplayName', 'Steady-State LICOR')
+    plot(data.T, data.CB, 'g.', 'DisplayName', 'DAQ Chamber');
+    plot(data.T, data.CA, 'r.', 'DisplayName', 'DAQ Chamber');
+    plot(data.T, data.C, 'c.', 'DisplayName', 'LICOR')
     ylabel('CO_2 [ppm]');
     legend();
-    title(['Steady State Data-Points of Flux Test Dataset ' dataset]);
+    title(["Steady State Indices - Delivering " + map{dataset_idx, 4} + " μmol/m^2/s", "[DATASET " + dataset + "]"]);
     xlabel('Time');
     hold off;
     
+    % calculate flux
+    data.F = ((lpm_to_cms(data.Q./1000).*ppm_to_mol(data.CB-data.CA))./As).*1e6;
+    data.F_FLOOR = ((lpm_to_cms(data.Q./1000).*ppm_to_mol(data.CB_FLOOR-data.CA_FLOOR))./As).*1e6;
+    data.F_LICOR = ((lpm_to_cms(data.Q./1000).*ppm_to_mol(data.C_FLOOR))./As).*1e6;
+
+    f_mean_ss = mean(data.F(cb_ss));
+    f_floor_mean_ss = mean(data.F_FLOOR(cb_ss));
+    f_licor_mean_ss = mean(data.F_LICOR(cb_ss));
+    f_std_ss = std(data.F(cb_ss));
+    f_floor_std_ss = std(data.F_FLOOR(cb_ss));
+    f_licor_std_ss = std(data.F_LICOR(cb_ss));
     
-    data.F = (lpm_to_cms(data.Q./1000).*ppm_to_mol(data.CB-data.CA))./As;
-    data.F_FLOOR = (lpm_to_cms(data.Q./1000).*ppm_to_mol(data.CB_FLOOR-data.CA_FLOOR))./As;
-    data.F_LICOR = (lpm_to_cms(data.Q./1000).*ppm_to_mol(data.C_FLOOR))./As;
+    % plot fluxes
     figure();
     hold on;
-    plot(data.T(cb_ss), data.F(cb_ss), 'go', 'DisplayName', 'SS Flux');
-    plot(data.T(c_ss), data.F_LICOR(c_ss), 'co', 'DisplayName', 'SS Flux FLOOR LICOR');
-    plot(data.T, data.F, 'r.', 'DisplayName', 'Flux');
-    plot(data.T, data.F_FLOOR, 'm.', 'DisplayName', 'Flux FLOOR');
-    plot(data.T, data.F_LICOR, 'b.', 'DisplayName', 'Flux FLOOR LICOR');
+    plot(data.T(cb_ss), data.F(cb_ss), 'g', 'DisplayName', "SS Flux, μ: " + f_mean_ss + " σ: " + f_std_ss + " μmol/m^2/s");
+    plot(data.T(c_ss), data.F_LICOR(c_ss), 'r', 'DisplayName', "SS Flux FLOOR LICOR, μ: " + f_licor_mean_ss + " σ: " + f_licor_std_ss + " μmol/m^2/s");
+    plot(data.T(c_ss), data.F_FLOOR(c_ss), 'c', 'DisplayName', "SS Flux FLOOR, μ: " + f_floor_mean_ss + " σ: " + f_floor_std_ss + " μmol/m^2/s");
+    yline(map{dataset_idx, 4}, 'k--', 'DisplayName', "Delivered Flux")
     ylabel('CO_2 Flux μmol/m^2/s');
     legend();
-    title(['Flux Data-Points of Flux Test Dataset ' dataset]);
+    title(["Steady State Flux Results - Delivering " + map{dataset_idx, 4} + " μmol/m^2/s", "[DATASET " + dataset + "]"]);
     xlabel('Time');
     hold off;
-    
+
+    % plot fluxes
+    figure();
+    hold on;
+    plot(data.T, data.F, 'g', 'DisplayName', "Flux");
+    plot(data.T, data.F_LICOR, 'r', 'DisplayName', "Flux FLOOR LICOR");
+    plot(data.T, data.F_FLOOR, 'c', 'DisplayName', "Flux FLOOR");
+    yline(map{dataset_idx, 4}, 'k--', 'DisplayName', "Delivered Flux")
+    ylabel('CO_2 Flux μmol/m^2/s');
+    legend();
+    title(["Flux Results - Delivering " + map{dataset_idx, 4} + " μmol/m^2/s", "[DATASET " + dataset + "]"]);
+    xlabel('Time');
+    hold off;
     
 end
 
