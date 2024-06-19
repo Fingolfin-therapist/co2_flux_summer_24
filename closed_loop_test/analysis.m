d740674da0cda2f00836e65ec666ea33c11dcad4
Lincoln Scheer <63661070+BikeLinc@users.noreply.github.com>
2024-06-19 15:03:39 -0600

    Add Extended Closed Loop Dataset

--- closed_loop_test/analysis.m
+++ closed_loop_test/analysis.m
@@ -5,9 +5,9 @@
 %
 
 clc, clear, close all
-set(groot,'defaulttextinterpreter','tex');  
-set(groot, 'defaultAxesTickLabelInterpreter','tex');  
-set(groot, 'defaultLegendInterpreter','tex');
+% set(groot,'defaulttextinterpreter','tex');  
+% set(groot, 'defaultAxesTickLabelInterpreter','tex');  
+% set(groot, 'defaultLegendInterpreter','tex');
 
 %% Import Datasets
 
@@ -15,78 +15,108 @@ set(groot, 'defaultLegendInterpreter','tex');
 daq = IMPORTDAQFILE("daq.csv");
 licor = IMPORTLICORFILE("licor.txt");
 
+% split daq datasets into two seperate datasets, so that if one sensor goes
+% offline we can still create a strong calibration with a sensor with more
+% data.
+daq.Q = [];
+daqa = daq;
+daqb = daq;
+daqa.CB = [];
+daqa.TB = [];
+daqa.HB = [];
+daqb.CA = [];
+daqb.TA = [];
+daqb.HA = [];
+
+
+
+%%
 % remove rows that show the ELTs throwing errors
-elt_errors = [500, 2815, 64537, 231753, 65535];
+elt_errors = [500, 2815, 64537, 231753, 65535, 2500, 2559];
 for elt_error = elt_errors
-    daq_idx = daq.CA ~= elt_error;
-    daq = daq(daq_idx, :);
-    daq_idx = daq.CB ~= elt_error;
-    daq = daq(daq_idx, :);
+    daq_idx = daqa.CA ~= elt_error;
+    daqa = daqa(daq_idx, :);
+    daq_idx = daqb.CB ~= elt_error;
+    daqb = daqb(daq_idx, :);
 end
 
 %% plot raw sensor data
 figure();
 hold on;
-plot(daq.T, daq.CA, 'DisplayName', 'ELT CO_2 A');
-plot(daq.T, daq.CB, 'DisplayName', 'ELT CO_2 B');
+plot(daqa.T, daqa.CA, 'DisplayName', 'ELT CO_2 A');
+plot(daqb.T, daqb.CB, 'DisplayName', 'ELT CO_2 B');
 plot(licor.T, licor.C, 'DisplayName', 'LICOR CO_2');
 legend();
 title("Closed Loop Calibration - Raw Sensor Data");
 
 %% smooth data over 5 mins
 
-smooth_dt = minutes(5);
+smooth_dt = minutes(60);
+retime_dt = minutes(1);
 
 % retime datasets
-daq = retime(daq,"regular", 'mean', 'TimeStep', seconds(30));
-licor = retime(licor,"regular", 'mean', 'TimeStep', seconds(30));
+daqa = retime(daqa,"regular", 'mean', 'TimeStep', retime_dt);
+daqb = retime(daqb,"regular", 'mean', 'TimeStep', retime_dt);
+licor = retime(licor,"regular", 'mean', 'TimeStep', retime_dt);
 
 % apply moving mean
-daq = rmoutliers(daq, 'percentile', [20 80]);
+daqa = rmoutliers(daqa, 'percentile', [20 80]);
+daqb = rmoutliers(daqb, 'percentile', [20 80]);
 licor = rmoutliers(licor, 'percentile', [20 80]);
 
 % apply moving mean
-daq = smoothdata(daq, 'movmean', minutes(5));
-licor = smoothdata(licor, 'movmean', minutes(5));
+daqa = smoothdata(daqa, 'movmean', smooth_dt);
+daqb = smoothdata(daqb, 'movmean', smooth_dt);
+licor = smoothdata(licor, 'movmean', smooth_dt);
 
 
 
-data = synchronize(daq, licor);
+dataa = synchronize(daqa, licor);
+datab = synchronize(daqb, licor);
+
+dataa = rmmissing(dataa);
+datab = rmmissing(datab);
 
 % plot smoothed data
 
 figure();
 hold on;
-plot(daq.T, daq.CA, 'DisplayName', 'ELT CO_2 A');
-plot(daq.T, daq.CB, 'DisplayName', 'ELT CO_2 B');
+plot(dataa.T, dataa.CA, 'DisplayName', 'ELT CO_2 A');
+plot(datab.T, datab.CB, 'DisplayName', 'ELT CO_2 B');
 plot(licor.T, licor.C, 'DisplayName', 'LICOR CO_2');
 legend();
 title("Closed Loop Calibration - Hourly Smoothed Sensor Data");
 
 %% generate regressions
 
-CA_Model = fitlm(data.CA, data.C);
-CB_Model = fitlm(data.CB, data.C);
+CA_Model = fitlm(dataa.CA, dataa.C);
+CB_Model = fitlm(datab.CB, datab.C);
 
 
 % plot regressions
 
+text_size = 48;
+
 figure();
-subplot(1, 2, 1);
-plot(CA_Model);
+hold on
 
-txt = "RMSE: " + CA_Model.RMSE + "\newlineR^2: " + CA_Model.Rsquared.Ordinary
+plot(CA_Model.Variables.y, CA_Model.Variables.y, 'r--', 'LineWidth', 2);
+plot(CA_Model.Fitted, CA_Model.Variables.y, 'b.', 'MarkerSize', 20);
 
-text(min(xlim)+50, max(ylim)-50,  txt);
-xlabel("ELT A CO_2 [ppm]");
-ylabel("LICOR CO_2 [ppm]");
-title("ELT A Regression");
+txt = "RMSE: " + CA_Model.RMSE + " [ppm CO_2]\newlineR^2: " + CA_Model.Rsquared.Ordinary;
+
+text(min(xlim)+50, max(ylim)-50,  txt, 'FontSize', text_size,'Interpreter','tex');
+xlabel("ELT A CO_2 [ppm CO_2]", 'FontSize', text_size,'Interpreter','tex');
+ylabel("LICOR CO_2 [ppm CO_2]", 'FontSize', text_size,'Interpreter','tex');
+title('Linear Regression for Calibrating ELT S300-3V CO_2 Sensors','Interpreter','tex', 'FontSize', text_size);
+legend(["Fitted CO_2 Dataset","1:1 Fit"],'FontSize',text_size/2, 'Interpreter', 'tex');
 
 
-subplot(1, 2, 2);
+
+figure();
 plot(CB_Model);
 
-txt = "RMSE: " + CB_Model.RMSE + "\newlineR^2: " + CB_Model.Rsquared.Ordinary
+txt = "RMSE: " + CB_Model.RMSE + "\newlineR^2: " + CB_Model.Rsquared.Ordinary;
 
 text(min(xlim)+50, max(ylim)-50,  txt);
 xlabel("ELT B CO_2 [ppm]");
